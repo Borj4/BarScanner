399372b65d724634024f8e61e99e281db7443a44
Borj4 <river4car@gmail.com>
2021-07-06 10:24:11 +0200

    trying in functional

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
+
+//Tienes que ver qué se hace con _onDetected
+
 import Quagga from 'quagga';
 
+const Scanner = () => { 
+// export default class Scanner extends Component {
+//     constructor(props) {
+//         super(props);
+//         this._onDetected = this._onDetected.bind(this);
+//     }
 
-export default class Scanner extends Component {
-    constructor(props) {
-        super(props);
-        this._onDetected = this._onDetected.bind(this);
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         Quagga.init(
             {
                 inputStream: {
@@ -37,17 +41,48 @@ export default class Scanner extends Component {
             }
         );
         Quagga.onDetected(this._onDetected);
-    }
+        return () => Quagga.offDetected(this._onDetected)
+    }, [])
+    // componentDidMount() {
+        // Quagga.init(
+        //     {
+        //         inputStream: {
+        //             type: 'LiveStream',
+        //             constraints: {
+        //                 width: 640,
+        //                 height: 480,
+        //                 facingMode: 'environment', // or user
+        //             },
+        //         },
+        //         locator: {
+        //             patchSize: 'medium',
+        //             halfSample: true,
+        //         },
+        //         numOfWorkers: 0,
+        //         decoder: {
+        //             readers: ['ean_reader'],
+        //         },
+        //         locate: false,
+        //     },
+        //     function(err) {
+        //         if (err) {
+        //             return console.log(err);
+        //         }
+        //         Quagga.start();
+        //     }
+        // );
+        // Quagga.onDetected(this._onDetected);
+    // }
 
-    componentWillUnmount() {
-        Quagga.offDetected(this._onDetected);
-    }
+    // componentWillUnmount() {
+    //     Quagga.offDetected(this._onDetected);
+    // }
 
-    _onDetected(result) {
+    Quagga._onDetected((result) => {
         this.props.onDetected(result);
-    }
+    })
 
-    render() {
-        return <div id="interactive" className="viewport" />;
-    }
+
+    return  <div id="interactive" className="viewport" />;   
 }
+export default Scanner;
